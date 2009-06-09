8a9498224e37990740a696e62829bcbada133f5c
Tony Arcieri <tony@medioh.com>
2009-06-09 02:48:53 -0600

    Begin namespacing specs

--- spec/require_all_spec.rb
+++ spec/require_all_spec.rb
@@ -1,18 +1,20 @@
 require File.dirname(__FILE__) + '/../lib/require_all.rb'
 
 describe "require_all" do    
-  it "handles load ordering when dependencies are resolvable" do
-    require_all File.dirname(__FILE__) + '/fixtures/resolvable/*.rb'
+  describe "dependency resolution" do
+    it "handles load ordering when dependencies are resolvable" do
+      require_all File.dirname(__FILE__) + '/fixtures/resolvable/*.rb'
     
-    defined?(A).should == "constant"
-    defined?(B).should == "constant"
-    defined?(C).should == "constant"
-    defined?(D).should == "constant"
-  end
+      defined?(A).should == "constant"
+      defined?(B).should == "constant"
+      defined?(C).should == "constant"
+      defined?(D).should == "constant"
+    end
   
-  it "raises NameError if dependencies can't be resolved" do
-    proc do 
-      require_all File.dirname(__FILE__) + '/fixtures/unresolvable/*.rb'
-    end.should raise_error(NameError)
+    it "raises NameError if dependencies can't be resolved" do
+      proc do 
+        require_all File.dirname(__FILE__) + '/fixtures/unresolvable/*.rb'
+      end.should raise_error(NameError)
+    end
   end
 end
