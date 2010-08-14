6347433db0c5bee48d3165c4f390e71496a27717
Jarmo Pertman <jarmo.p@gmail.com>
2010-08-14 20:13:11 +0300

    added base_dir specs for autoload

--- spec/autoload_spec.rb
+++ spec/autoload_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/autoload_shared.rb'
 
 describe "autoload_all" do
   it "provides require_all functionality by using 'autoload' instead of 'require'" do
-    should_not be_loaded("Autoloaded::Module1::A")
+    should_not be_loaded("Autoloaded::Module1::A", "Autoloaded::Module2::LongerName", "Autoloaded::Module2::Module3::B")
     autoload_all File.dirname(__FILE__) + "/fixtures/autoloaded"
     should be_loaded("Autoloaded::Module1::A", "Autoloaded::Module2::LongerName", "Autoloaded::Module2::Module3::B")
   end
@@ -11,6 +11,17 @@ describe "autoload_all" do
   it "doesn't autoload files with wrong module names" do
     autoload_all File.dirname(__FILE__) + "/fixtures/autoloaded"
     should_not be_loaded("Autoloaded::WrongModule::WithWrongModule", "WrongModule::WithWrongModule")
+  end
+
+  it "needs to specify base_dir for autoloading if loading something from under top-level module directory" do
+    should_not be_loaded("Autoloaded::Module1::A", "Autoloaded::Module2::LongerName", "Autoloaded::Module2::Module3::B")
+    autoload_all File.dirname(__FILE__) + "/fixtures/autoloaded/module1"
+    should_not be_loaded("Autoloaded::Module1::A", "Autoloaded::Module2::LongerName", "Autoloaded::Module2::Module3::B")
+
+    autoload_all File.dirname(__FILE__) + "/fixtures/autoloaded/module1",
+                 :base_dir => File.dirname(__FILE__) + "/fixtures/autoloaded"
+    should be_loaded("Autoloaded::Module1::A")
+    should_not be_loaded("Autoloaded::Module2::LongerName", "Autoloaded::Module2::Module3::B")
   end
 
   before(:all) do
@@ -26,6 +37,17 @@ describe "autoload_rel" do
     should_not be_loaded("Modules::Module1::First", "Modules::Module2::Second", "Modules::Zero")
     require File.dirname(__FILE__) + '/fixtures/autoloaded_rel/modules/zero'
     should be_loaded("Modules::Module1::First", "Modules::Module2::Second", "Modules::Zero")
+  end
+
+  it "needs to specify base_dir for autoloading if loading something from under top-level module directory" do
+    should_not be_loaded("Autoloaded::Module1::A", "Autoloaded::Module2::LongerName", "Autoloaded::Module2::Module3::B")
+    autoload_rel "./fixtures/autoloaded/module1"
+    should_not be_loaded("Autoloaded::Module1::A", "Autoloaded::Module2::LongerName", "Autoloaded::Module2::Module3::B")
+
+    autoload_rel "./fixtures/autoloaded/module1",
+                 :base_dir => "./spec/fixtures/autoloaded"
+    should be_loaded("Autoloaded::Module1::A")
+    should_not be_loaded("Autoloaded::Module2::LongerName", "Autoloaded::Module2::Module3::B")
   end
 
   before(:all) do
